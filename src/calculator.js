45f896edf546ffddc051412ed77f0d93b38b45b9
Swapnil Gupta <swapnil89.don@gmail.com>
2015-12-13 23:09:04 +0530

    Initial commit

---
+++ src/calculator.js
@@ -0,0 +1,12 @@
+function add(x, y) {
+    function parseArg(n) {
+        if (Array.isArray(n)) 
+            return add.apply(this, n);
+        if (typeof n === "function") return parseArg(n());
+        return isNaN(n)? 0 : parseInt(n,10);
+    }
+    var result = 0;
+    for (var i=0; i< arguments.length; i++)
+        result += parseArg(arguments[i]);
+    return result;
+}
